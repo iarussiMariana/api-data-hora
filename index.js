52919561c2250f43ccaa44da87314aa54d23ab8d
iarussiMariana <mariarussi2000@gmail.com>
2026-04-30 10:00:35 -0300

    fix cors

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
+// rota principal (pra não dar "Cannot GET /")
+app.get('/', (req, res) => {
+  res.json({
+    mensagem: 'API de Data e Hora funcionando 🚀',
+    rota: '/data-hora'
+  });
+});
+
+// rota da atividade
 app.get('/data-hora', (req, res) => {
   const agora = new Date();
 
   res.json({
-    data: agora.toLocaleDateString(),
-    hora: agora.toLocaleTimeString()
+    data: agora.toLocaleDateString('pt-BR'),
+    hora: agora.toLocaleTimeString('pt-BR')
   });
 });
 
